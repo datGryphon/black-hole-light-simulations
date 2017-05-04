28e93ad4668b77d31f306ddd2bcdbe8755363fdb
datGryphon <gryphonwastler@gmail.com>
2017-05-03 21:40:52 -0400

    Made small changes to the program to conform with the spec document and make server deployment more user friendly

--- serve.py
+++ serve.py
@@ -1,35 +1,44 @@
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, Response, jsonify
+     abort, render_template, flash, Response, jsonify, make_response
 from werkzeug.utils import secure_filename
-from flask_cors import CORS, cross_origin
 import subprocess
 from orbit import orbit
 from horizon import horizon
 from horizon import plot as BHplotname
-from flask import make_response
-from functools import wraps, update_wrapper
-from datetime import datetime
+from os import path
 
-rng=range(0,len(orbit),100)
+#########ADMIN DEFINED CONSTANTS##############
+ORBIT_STEP=1
+HORIZ_STEP=100
+BH_STEP=100
+PHOTON_STEP=100
+#default HOST is '0.0.0.0' broadcasts on all available IP's including loopback
+HOST='localhost'	
+#5000 is default for development purposes... Ports 0 to 1023 are reserved for the system
+PORT=5000
+##############################################
+
+DUDPHI=path.join(path.split(path.abspath(__file__))[0],"dudphi")
+
+rng=range(0,len(orbit),ORBIT_STEP)
 rng.append(-1)
 orbit = [orbit[i] for i in rng]
 
 orbitplotname = ["Photon" for i in range(len(orbit))]
 
-rng=range(0,len(horizon),100)
+rng=range(0,len(horizon),HORIZ_STEP)
 rng.append(-1)
 horizon = [horizon[i] for i in rng]
 
-rng=range(0,len(BHplotname),100)
+rng=range(0,len(BHplotname),BH_STEP)
 rng.append(-1)
 BHplotname = [BHplotname[i] for i in rng]
 
-# configuration
-DEBUG = True
+# configuration ----------Comment out the following line before deployment
+# DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-CORS(app)
 
 @app.route('/',methods=['GET'])
 def home():
@@ -62,11 +71,11 @@ def getPoints():
 
 		if not((radius==3.0 and sigma==90.0) or (radius<=2.0)):
 
-			output=subprocess.check_output(["./dudphi",str(radius),str(sigma)])
+			output=subprocess.check_output([DUDPHI,str(radius),str(sigma)])
 			photon=eval(output)
 			prepost=photon.pop(-1)
 			print "Process successfully called, exited with ", prepost
-			r= range(0,len(photon),100)
+			r= range(0,len(photon),PHOTON_STEP)
 			r.append(-1)
 			points= [ photon[i] for i in r]
 			plotlist = ["Photon" for i in range(len(points))]
@@ -87,13 +96,5 @@ def getPoints():
 		
 	return render_template('create.html',points=points,plotlist=plotlist,radius=radius,sigma=sigma)
 
-# @app.after_request
-# def after_request(response):
-# 	response.headers['Last-Modified'] = datetime.now()
-# 	response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-# 	response.headers['Pragma'] = 'no-cache'
-# 	response.headers['Expires'] = '-1'
-# 	return response
-
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host=HOST,port=PORT)
